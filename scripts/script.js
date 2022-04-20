75b641b60f96651884ca0caa263bcc4ee67cb519
JoaoBembe <joao.bembe@discente.ufopa.edu.br>
2022-04-20 20:03:20 -0300

    SlideShow completo. Ajustes no codigo html

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,22 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
 let slideIndex = 0;
-showSlides(slideIndex);
 
-
+showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName("container-destinos");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
   setTimeout(showSlides, 5000);
 }
