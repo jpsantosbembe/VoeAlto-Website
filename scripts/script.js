03a133edc67b1937668bb4833f12bddce98bccb3
JoaoBembe <joao.bembe@discente.ufopa.edu.br>
2022-04-20 13:09:46 -0300

    Primeira Versao. Nav Bar Concluida e slide show dos destinos.

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
